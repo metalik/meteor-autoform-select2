03310e5b9173b48b7f77327e54871a59c47bf316
Jacky Wong <jacky.wong@touchhk.com>
2015-04-21 22:55:21 +0800

    Update to autoform 5.1

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.use('templating@1.0.0');
   api.use('blaze@2.0.0');
   api.use('aldeed:template-extension@3.4.3');
-  api.use('aldeed:autoform@4.0.0 || 5.0.0');
+  api.use('aldeed:autoform@4.0.0 || 5.1.0');
   api.addFiles([
     'autoform-select2.html',
     'autoform-select2.js'
